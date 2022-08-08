3489aafcf65d3b273aa1873732cf2bd3a80f0e28
Annie Wahl <pyravice@gmail.com>
2022-08-08 17:51:30 -0600

    Moved backend to its own repo.

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: 'y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    USER: 'b8pc5ftb5i5uiob9',
+    PASSWORD: 'cw6dei91yfr3jl6p',
+    DB: 'c5qqa0w8f579tnyp',
+    dialect: "mysql",
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
