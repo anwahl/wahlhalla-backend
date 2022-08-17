af2cabafcfa966d3addb816049e0ccaab24f6d39
Annie Wahl <pyravice@gmail.com>
2022-08-16 22:00:34 -0600

    Fixed things and stuff.

--- app/routes/target.routes.js
+++ app/routes/target.routes.js
@@ -9,7 +9,7 @@ module.exports = app => {
     router.get("/:id", validator.validateID, target.findOne);
     router.put("/:id", validator.validateID, validator.validateTarget, target.update);
     router.delete("/:id", validator.validateID, target.delete);
-    router.get("/type/:type", validator.validateTypeID, target.findByType);
+    router.get("/type/:typeId", validator.validateTypeID, target.findByType);
     router.get("/description/:description", validator.validateDescription, target.findByDescription);
     app.use('/api/target', router);
 };
